c25af599747a768603b81503c1b80b5bea024084
Joe Wood <joewoodconsulting@outlook.com>
2017-09-17 07:45:25 -0400

    Package update

--- src/particle-canvas.tsx
+++ src/particle-canvas.tsx
@@ -1,119 +1,118 @@
 import * as React from "react";
-import Particles from "./particles";
-// import Color = require("color");
-import { IParticleEdge } from './particles'
-import { IParticleStyle } from "./model"
-import { keyBy, map, Dictionary } from "lodash"
+import { keyBy, map, Dictionary } from "lodash";
+import Particles,{ IParticleEdge } from "./particles";
+import { IParticleStyle } from "./model";
 
-import ParticleEdge, { ParticleScheduleState } from "./particle-edge"
+import ParticleEdge, { ParticleScheduleState } from "./particle-edge";
 
 export interface IProps {
-    style: {
-        width: number;
-        height: number;
-        backgroundColor?: string;
-    }
-    /** The default values for the particle style in each edge  */
-    particleStyle?: IParticleStyle;
-    defaultRatePerSecond?: number;
-    children?: ParticleEdge[];
-    run?: boolean;
+	style: {
+		width: number;
+		height: number;
+		backgroundColor?: string;
+	};
+	/** The default values for the particle style in each edge  */
+	particleStyle?: IParticleStyle;
+	defaultRatePerSecond?: number;
+	children?: ParticleEdge[];
+	run?: boolean;
 }
 
-export interface IState {
-}
+export interface IState {}
 
 export class ParticleCanvas extends React.PureComponent<IProps, IState> {
-    private canvas: HTMLCanvasElement;
-    private particles: Particles;
-    private edgeState: Dictionary<ParticleScheduleState> = {};
+	private canvas: HTMLCanvasElement;
+	private particles: Particles;
+	private edgeState: Dictionary<ParticleScheduleState> = {};
 
+	private setupParticles = (props: IProps) => {
+		if (!this.canvas) return;
+		const edgeChildren = keyBy(
+			React.Children.toArray(props.children) as { key: string; props: IParticleEdge }[],
+			c => c.key
+		);
+		map(edgeChildren, (v, k) => {
+			const style = { ...this.props.particleStyle, ...v.props.particleStyle };
+			if (!this.edgeState[k]) {
+				this.edgeState[k] = new ParticleScheduleState({ ...v.props, particleStyle: style });
+			} else {
+				this.edgeState[k].updateProps({ ...v.props, particleStyle: style });
+			}
+		});
+		this.particles.updateBuffers(Object.values(this.edgeState), props.style.width, props.style.height);
+		this.particles.draw();
+		if (props.run) this.particles.start();
+	};
 
-    private setupParticles = (props: IProps) => {
-        if (!this.canvas) return;
-        //set-up canvas
-        // this.particles.stop();
+	public componentWillReceiveProps(newProps: IProps) {
+		if (!!this.particles) {
+			if (
+				this.props.children !== newProps.children ||
+				this.props.style.width != newProps.style.width ||
+				this.props.style.height != newProps.style.height
+			) {
+				this.setupParticles(newProps);
+			}
+			if (newProps.run !== this.props.run) {
+				if (newProps.run) this.particles.start();
+				else this.particles.stop();
+			}
+		}
+	}
 
-        const edgeChildren = keyBy(React.Children.toArray(props.children) as { key: string, props: IParticleEdge }[], c => c.key);
-        map(edgeChildren, (v, k) => {
-            const style = {...this.props.particleStyle, ...v.props.particleStyle};
-            if (!this.edgeState[k]) {
-                this.edgeState[k] = new ParticleScheduleState({...v.props,particleStyle:style});
-            } else {
-                this.edgeState[k].updateProps({...v.props,particleStyle:style});
-            }
-            // console.log("Style",{...this.props.defaultParticleStyle,...v.props})
-        });
-        // this.particles.updateProps({ backgroundColor: props.style.backgroundColor, canvas: this.canvas });
-        this.particles.updateBuffers(Object.values(this.edgeState), props.style.width, props.style.height);
-        this.particles.draw();
-        if (props.run) this.particles.start();
-    }
+	public shouldComponentUpdate(newProps: IProps, _newState: IState) {
+		// if just run changes then don't update
+		if (
+			newProps.children == this.props.children &&
+			newProps.style.backgroundColor === this.props.style.backgroundColor &&
+			newProps.style.height === this.props.style.height &&
+			newProps.style.width === this.props.style.width
+		)
+			return false;
+		return true;
+	}
 
-    public componentWillReceiveProps(newProps: IProps) {
-        if (!!this.particles) {
-            if (this.props.children !== newProps.children || this.props.style.width != newProps.style.width || this.props.style.height != newProps.style.height) {
-                this.setupParticles(newProps);
-            }
-            if (newProps.run !== this.props.run) {
-                if (newProps.run)
-                    this.particles.start();
-                else
-                    this.particles.stop();
-            }
-        }
-    }
+	public componentWillUnmount() {
+		if (!!this.particles) {
+			this.particles.stop();
+		}
+	}
 
-    public shouldComponentUpdate(newProps: IProps, _newState: IState) {
-        // if just run changes then don't update
-        if (newProps.children == this.props.children &&
-            newProps.style.backgroundColor === this.props.style.backgroundColor &&
-            newProps.style.height === this.props.style.height &&
-            newProps.style.width === this.props.style.width) return false;
-        return true;
-    }
+	private lostContext = event => {
+		event.preventDefault();
+		const particles = this.particles;
+		this.particles = null;
+		particles.stop();
+		console.log("CONTEXT LOST IN CODE");
+	};
 
-    public componentWillUnmount() {
-        if (!!this.particles) {
-            this.particles.stop();
-        }
-    }
+	private setupNewCanvas = (canvas: HTMLCanvasElement) => {
+		if (this.canvas === canvas || !canvas) return;
+		if (this.canvas !== canvas) {
+			this.canvas = canvas;
+			this.canvas.addEventListener("webglcontextlost", this.lostContext, false);
+			this.canvas.addEventListener("webglcontextrestored", () => this.setupNewCanvas(this.canvas), false);
+		}
+		if (this.particles) {
+			this.particles.stop();
+			this.particles = null;
+		}
+		if (!this.particles) {
+			this.particles = new Particles({ canvas: this.canvas, size: 2 });
+		}
+		this.setupParticles(this.props);
+	};
 
-    private lostContext = (event) => {
-        event.preventDefault();
-        const particles = this.particles;
-        this.particles = null;
-        particles.stop();
-        console.log("CONTEXT LOST IN CODE");
-    }
-
-    private setupNewCanvas = (canvas: HTMLCanvasElement) => {
-        if (this.canvas === canvas || !canvas) return;
-        if (this.canvas !== canvas) {
-            this.canvas = canvas;
-            this.canvas.addEventListener("webglcontextlost", this.lostContext, false);
-            this.canvas.addEventListener("webglcontextrestored", () =>
-                this.setupNewCanvas(this.canvas), false);
-        }
-        if (this.particles) {
-            this.particles.stop();
-            this.particles = null;
-        }
-        if (!this.particles) {
-            this.particles = new Particles({ canvas: this.canvas, size: 2 });
-        }
-        this.setupParticles(this.props);
-    }
-
-    public render() {
-        const { width, height, backgroundColor } = this.props.style;
-        return (
-            <canvas key="webgl-canvas"
-                style={{ pointerEvents: "none", backgroundColor: backgroundColor || "#200020" }}
-                ref={this.setupNewCanvas}
-                width={width}
-                height={height}>
-            </canvas>);
-    }
+	public render() {
+		const { width, height, backgroundColor } = this.props.style;
+		return (
+			<canvas
+				key="webgl-canvas"
+				style={{ pointerEvents: "none", backgroundColor: backgroundColor || "#200020" }}
+				ref={this.setupNewCanvas}
+				width={width}
+				height={height}
+			/>
+		);
+	}
 }
-
--- src/particle-edge.ts
+++ src/particle-edge.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { IParticleEdge } from './particles'
+import { IParticleEdge } from "./particles";
 
 /**
  * Design:
@@ -14,81 +14,77 @@ import { IParticleEdge } from './particles'
  * 
  */
 
-
 export interface IParticleSchedule {
-    appliesFrom: Date;
-    particles: number[];
-    ratePerSecond: number;
-    props: IParticleEdge;
-    last: {
-        appliesFrom: Date;
-        end: Date;
-        ratePerSecond: number;
-        particles: number[]
-    }
+	appliesFrom: Date;
+	particles: number[];
+	ratePerSecond: number;
+	props: IParticleEdge;
+	last: {
+		appliesFrom: Date;
+		end: Date;
+		ratePerSecond: number;
+		particles: number[];
+	};
 }
 
 export default class ParticleEdge extends React.Component<IParticleEdge, any> {
+	constructor(props: IParticleEdge) {
+		super(props);
+		console.log("New Particle Edge");
+	}
 
-    constructor(props: IParticleEdge) {
-        super(props);
-        console.log("New Particle Edge")
-    }
-
-    public render() {
-        return null;
-    }
+	public render() {
+		return null;
+	}
 }
-
 
 /** Holds the historic state of ParticleEdge property changes. Offers a schedule of properties changes 
  * for animations to gradually be applied.
  */
 export class ParticleScheduleState {
+	private last: {
+		ratePerSecond: number;
+		end: Date;
+		start: Date;
+	} = null;
+	private randomSample: number[];
 
-    private last: {
-        ratePerSecond: number;
-        end: Date;
-        start: Date;
-    } = null;
-    private randomSample: number[];
+	private edge: IParticleEdge;
 
-    private edge: IParticleEdge;
+	constructor(props: IParticleEdge) {
+		this.edge = Object.assign({}, props);
+		this.randomSample = [];
+		for (let n = 10000; n >= 0; n--) {
+			this.randomSample[n] = Math.random();
+		}
+		this.last = null;
+	}
 
-    constructor(props: IParticleEdge) {
-        this.edge = Object.assign({}, props);
-        this.randomSample = [];
-        for (let n = 10000; n >= 0; n--) { this.randomSample[n] = Math.random(); }
-        // this.randomSample = this.randomSample.sort((a, b) => a - b);
-        this.last = null;
-    }
+	public getParticles(): IParticleSchedule {
+		return {
+			appliesFrom: (this.last && this.last.end) || new Date(new Date().valueOf() - 0),
+			particles: this.randomSample,
+			ratePerSecond: this.edge.ratePerSecond,
+			props: this.edge,
+			last: this.last && {
+				appliesFrom: this.last.start,
+				end: this.last.end,
+				ratePerSecond: this.last.ratePerSecond,
+				particles: this.randomSample
+			}
+		};
+	}
 
-    public getParticles(): IParticleSchedule {
-        return {
-            appliesFrom: (this.last && this.last.end) || new Date(new Date().valueOf() - 0),
-            particles: this.randomSample,
-            ratePerSecond: this.edge.ratePerSecond,
-            props: this.edge,
-            last: this.last && {
-                appliesFrom: this.last.start,
-                end: this.last.end,
-                ratePerSecond: this.last.ratePerSecond,
-                particles: this.randomSample
-            }
-        }
-    }
-
-    /** Property change has occurred */
-    public updateProps(newProps: IParticleEdge) {
-        const now = new Date();
-        if (newProps.ratePerSecond != this.edge.ratePerSecond) {
-            this.last = {
-                ratePerSecond: this.edge.ratePerSecond,
-                start: (this.last && this.last.end) || new Date(now.valueOf() - 1000),
-                end: now
-            };
-        }
-        this.edge = { ...this.edge, ...newProps };
-    }
-
+	/** Property change has occurred */
+	public updateProps(newProps: IParticleEdge) {
+		const now = new Date();
+		if (newProps.ratePerSecond != this.edge.ratePerSecond) {
+			this.last = {
+				ratePerSecond: this.edge.ratePerSecond,
+				start: (this.last && this.last.end) || new Date(now.valueOf() - 1000),
+				end: now
+			};
+		}
+		this.edge = { ...this.edge, ...newProps };
+	}
 }
